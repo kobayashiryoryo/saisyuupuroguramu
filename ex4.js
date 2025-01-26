f1173c2dc2a3947f158796468949aa72ed403435
kobayashiryoryo <167833061+kobayashiryoryo@users.noreply.github.com>
2025-01-26 19:09:58 +0900

    Update ex4.js

--- ex4.js
+++ ex4.js
@@ -1,2 +1,2 @@
 function hyoji1(){document.getElementById('log').src='./image/1000_1000_102400.jpg'; setTimeout("hyoji2()",3000);}
-function hyoji2(){document.getElementById('log').src='./image/image/83c331516cebd23db02cdf4df678cdd4_t.jpeg';}
+function hyoji2(){document.getElementById('log').src='./image/83c331516cebd23db02cdf4df678cdd4_t.jpeg';}
