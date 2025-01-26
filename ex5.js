8ab075617b3d49611a6785e8264f8178f58b2235
kobayashiryoryo <167833061+kobayashiryoryo@users.noreply.github.com>
2025-01-26 22:17:06 +0900

    Update ex5.js

--- ex5.js
+++ ex5.js
@@ -1,7 +1,1 @@
-const images=['./image/re2.jpg', './image/mobitQP1O.jpg', './image/mobIDBsat.jpg', './image/c96f3813674b9861bd92a7f98f12c.png', './image/7c0b66287e152e72f051f6581147c-02.jpg'];
-let currentIndex=0;
-
-function changeImage(){const imgElement=document.getElementById("slideshow");
-                       currentIndex=(currentINdex + 1) % images.length;
-                       imgElement.src=images[currentIndex];}
-setInterval(changeImage,2000);
+function hyoji3(){document.getElementById('lo').src='./image/
