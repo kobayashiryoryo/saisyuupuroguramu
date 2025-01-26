8a6dba5cbdd903cf37b0fae23678722249559229
kobayashiryoryo <167833061+kobayashiryoryo@users.noreply.github.com>
2025-01-26 23:09:53 +0900

    Update ex5.js

--- ex5.js
+++ ex5.js
@@ -1,2 +1,2 @@
-function hyoji3(){document.getElementById('lo').src='./image/re2.jpg'; setTimeout("hyoji4()",2000);}
-function hyoji4(){document.getElementById('lo').src='./image/mobitQP1O.jpg';｝
+function hyoji3(){document.getElementById('lo').src='./image/mobIDBsat.jpg'; setTimeout("hyoji4()",2000);}
+function hyoji4(){document.getElementById('lo').src='./image/re2.jpg';｝
