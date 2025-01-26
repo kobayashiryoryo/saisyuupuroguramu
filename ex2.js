299f74ba0679560d61893f3a6626d7d84ee4e5e3
kobayashiryoryo <167833061+kobayashiryoryo@users.noreply.github.com>
2025-01-26 17:23:31 +0900

    Update ex2.js

--- ex2.js
+++ ex2.js
@@ -1,2 +1,2 @@
 function imgchange1(){document.getElementById('logo').src='./image/R.jpg';}
-function imgchange2(){document.getElementById('logo').src='./image
+function imgchange2(){document.getElementById('logo').src='./image/topslide-silentmajority.jpg';}
