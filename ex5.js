0faaef33a0c4e4487d7aed83f594e841941b0d67
kobayashiryoryo <167833061+kobayashiryoryo@users.noreply.github.com>
2025-01-26 21:03:39 +0900

    Update ex5.js

--- ex5.js
+++ ex5.js
@@ -1,1 +1,7 @@
+const images=["re2.jpg", "mobitQP1O.jpg", "mobIDBsat.jpg", "c96f3813674b9861bd92a7f98f12c.png", "7c0b66287e152e72f051f6581147c-02.jpg"]
+let currentIndex=0;
 
+function changeImage(){const imgElement=document.getElementById("slideshow");
+                       currentIndex=(currentINdex + 1) % images.length;
+                       imgElement.src=images[currentIndex];}
+setInterval(changeImage,2000);
